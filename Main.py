9b30e2ffebadb275f3c2221f9f4a961fccd49322
Fyrlian <darymartin@gmail.com>
2025-08-09 02:08:19 +0200

    added AI powered analysis to the app (untested)

--- Main.py
+++ Main.py
@@ -3,25 +3,43 @@ from tkinter import filedialog # filedialog for selecting folders
 import requests # requests library for conections with OLlama
 from PIL import ImageGrab as sc # imports the screenshot function from pillow library
 import os # imports the os library for file operations
+from ollama import Ollama # imports the Ollama library to send requests to analyze champ select
 # -------------- FUNCTIONS --------------
 
 # screenshots the league client
 def captureScreenshot():
     image = sc.grab() # takes the screenshot to analyze
-    saveRoute = os.path.join(saveFolder, "screenshot.png") # sets the path to save the screenshot
     image.save(saveRoute)
 
 # allows selecting the folder to save images
 def selectSaveFolder():
     newFolder = filedialog.askdirectory(title="Please select the folder to save images") # opens a dialog to select folder
     if newFolder:
-        global saveFolder
-        saveFolder = newFolder
-        folderTag.config(text=f"Current Save Folder: {saveFolder}")
+        global saveFolder # allows access to saveFolder variable
+        global saveRoute # allows access to saveRoute variable
+
+        saveFolder = newFolder # modifies saveFolder to be updated
+        saveRoute = os.path.join(saveFolder, "screenshot.png") # sets the path to save the screenshot
+
+        folderTag.config(text=f"Current Save Folder: {saveFolder}") # updates the label to show the new save folder
+
+# analyzes the champions in the screenshot using llava model
+def analyzeScreenshot():
+    response = ollama.chat(
+        model = "llava",
+        messages = [
+            {"role":"user", "content": systemPrompt,"image":saveRoute}
+        ]
+    )
+
+    print(response['choices'][0]['message']['content']) # prints the response from the model
 
 # -------------- CONFIGS --------------
 
-saveFolder = os.path.join(os.path.expanduser("~"), "Pictures") # initial image saving route
+saveFolder = os.path.join(os.path.expanduser("~"), "Pictures") # initial folder for the images
+saveRoute = os.path.join(saveFolder, "screenshot.png") # sets the path initial route to save the screenshot
+ollama = Ollama() # creates an instance of the Ollama class to send requests
+systemPrompt = "Analyze the following screenshot for League of Legends champion select:" 
 
 # -------------- WINDOW --------------
 
